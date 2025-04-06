22d64a77885fa90e35e83742a9590d07b33384c6
martinsllm <lucaslm2010@gmail.com>
2025-04-06 18:38:14 -0300

    Changing function to complete task to models file

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,3 @@
-from datetime import date
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, redirect
@@ -30,6 +29,5 @@ class TodoDeleteView(DeleteView):
 class TodoCompleteView(View):
     def get(self, request, pk):
         todo = get_object_or_404(Todo, pk=pk)
-        todo.finished_at = date.today()
-        todo.save()
+        todo.mark_has_complete()
         return redirect("todo_list")
