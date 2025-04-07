386120763b105d46d2a05cec4751592b12cb009d
martinsllm <lucaslm2010@gmail.com>
2025-04-06 22:36:56 -0300

    Removing unused url

--- setup/urls.py
+++ setup/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView, TodoCompleteView
 
-urlpatterns = [path("admin/", admin.site.urls),
-               path("", TodoListView.as_view(), name="todo_list"),
+urlpatterns = [path("", TodoListView.as_view(), name="todo_list"),
                path("create", TodoCreateView.as_view(), name="todo_create"),
                path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
                path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
